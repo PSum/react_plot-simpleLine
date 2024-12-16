2701b6d3675068255146ec7456a9d47f6c662983
Philipp Sum <sum.philipp@gmail.com>
2024-12-16 20:52:46 +0100

    update
    making the example a bit more complex and interactive

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { LineChart, Line } from 'recharts'
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +12,24 @@ function App() {
     <>
   <LineChart width={400} height={400} data={data}>
     <Line type="monotone" dataKey="y" stroke="#8884d8" />
+    {
+    // Adding a dashed grid in the background
+    }
+    <CartesianGrid stroke='#ccc' strokeDasharray={'5 5'} />
+
+    {// X axis with datakey of the dataset
+  }
+    <XAxis dataKey={'x'} />
+    {
+
+    // Without the following line, there is no y axis
+    }
+    <YAxis />
+
+    {
+    // Tooltip shows information of the data when hovering over it
+    }
+    <Tooltip></Tooltip>
   </LineChart>
     </>
   )
